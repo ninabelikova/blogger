8b5e6a5274d9678bade622025c5b8e7b048c62ed
ninabelikova <saraynachan@mail.ru>
2015-09-07 21:56:33 +0800

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+    
     def index
         @articles = Article.all
     end
@@ -6,4 +8,39 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        flash.notice = "Hababa prayer '#{@article.title}' created! You just contributed to the Hababa religion!"
+
+        redirect_to article_path(@article)
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash.notice = "Oh no :( '#{@article.title}' is now destroyed!"
+        redirect_to articles_path
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        
+        flash.notice = "Hababa prayer '#{@article.title}' updated! Are you happy now?"
+
+        redirect_to article_path(@article)
+    end
+    
+
 end
+
